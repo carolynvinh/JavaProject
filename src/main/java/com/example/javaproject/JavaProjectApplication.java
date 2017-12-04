2e5b237e720341720227c1a8ebd9805b75508aa2
Dayaram <vdayaram@NYCLA2AXHTD8.local>
2017-12-04 16:47:31 -0500

    cleaned up the code, and added exceptions as well, everything is functioning

--- src/main/java/com/example/javaproject/JavaProjectApplication.java
+++ src/main/java/com/example/javaproject/JavaProjectApplication.java
@@ -1,5 +1,4 @@
 package com.example.javaproject;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/example/javaproject/PortfolioController.java
+++ src/main/java/com/example/javaproject/PortfolioController.java
@@ -1,5 +1,4 @@
 package com.example.javaproject;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,7 +11,7 @@ import java.util.*;
 @RestController
 public class PortfolioController {
 
-    static Map<String, ArrayList<Stock>> map = new ConcurrentHashMap<>();
+    public static Map<String, ArrayList<Stock>> map = new ConcurrentHashMap<>();
 
     @RequestMapping(method = RequestMethod.GET, value = "/api/v1/portfolios")
     public Set<String> getPortfolios(){
@@ -20,75 +19,101 @@ public class PortfolioController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value ="/api/v1/portfolios/{portfolioName}")
-    public ArrayList<Stock> fetchPositions(@PathVariable("portfolioName") String portfolioName){
-        return map.get(portfolioName);
+    public ArrayList<Stock> fetchPositions(@PathVariable("portfolioName") String portfolioName) throws Exception {
+        if(map.containsKey(portfolioName)){
+            return map.get(portfolioName);
+        } else {
+            throw new Exception("The portfolio you are searching for does not exist");
+        }
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/api/v1/portfolios/net-asset-value")
     public ArrayList<Portfolio> netAssetValue(){
         ArrayList<Portfolio> portfolioList = new ArrayList<Portfolio>();
-
         map.forEach((k, v) -> {
             int sum = v.stream().mapToInt(stock -> (int) stock.getValue()).sum();
             Portfolio portfolioAndValue = new Portfolio(k, sum);
             portfolioList.add(portfolioAndValue);
         });
-
         return portfolioList;
     };
 
     @RequestMapping(method = RequestMethod.PUT, path = "/api/v1/portfolios/{portfolioName}")
-    public Map<String, ArrayList<Stock>> createPortfolio(@PathVariable("portfolioName") String portfolioName){
-        ArrayList<Stock> list = new ArrayList<Stock>();
-        map.put(portfolioName, list);
-        return map;
+    public Map<String, ArrayList<Stock>> createPortfolio(@PathVariable("portfolioName") String portfolioName) throws Exception {
+        if(map.containsKey(portfolioName)){
+            throw new Exception("The portfolio you entered already exists. Please select a new name.");
+        } else {
+            ArrayList<Stock> list = new ArrayList<Stock>();
+            map.put(portfolioName, list);
+            return map;
+        }
     }
-
 
     @RequestMapping(method = RequestMethod.PUT, value = "/api/v1/portfolios/{portfolioName}/{ticker}")
     public Map<String, ArrayList<Stock>> insertPosition(@PathVariable("portfolioName") String portfolioName,
                                                    @PathVariable("ticker") String ticker,
-                                                        @RequestParam(value="marketValue", required=true) double marketValue){
-        Stock stock = new Stock(ticker, marketValue);
-        map.get(portfolioName).add(stock);
-        return map;
+                                                        @RequestParam(value="marketValue", required=true) double marketValue) throws Exception {
+        if(map.containsKey(portfolioName)){
+            Stock stock = new Stock(ticker, marketValue);
+            map.get(portfolioName).add(stock);
+            return map;
+        } else {
+            throw new Exception("The portfolio you would like to add a position to does not exist.");
+        }
+
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/api/v1/portfolios/{portfolioName}/{ticker}")
     public Map<String, ArrayList<Stock>> updatePosition(@PathVariable("portfolioName") String portfolioName,
                                                         @PathVariable("ticker") String ticker,
-                                                        @RequestParam(value="marketValue", required=true) double marketValue){
-
+                                                        @RequestParam(value="marketValue", required=true) double marketValue) throws Exception {
         ArrayList portfolio = map.get(portfolioName);
-
+        boolean changed = false;
         for(int i = 0; i < portfolio.size(); i++){
             Stock position = (Stock) portfolio.get(i);
             if((position.getName()).equals(ticker)){
                 position.setValue(marketValue);
+                changed = true;
             }
         }
-        return map;
+        if(changed){
+            return map;
+        } else {
+            throw new Exception("The position you are searching for does not exist in this portfolio. Please try again.");
+        }
     }
 
     @RequestMapping(method = RequestMethod.DELETE, path = "/api/v1/portfolios/{portfolioName}")
-    public Map<String, ArrayList<Stock>> deletePortfolio(@PathVariable("portfolioName") String portfolioName){
-        map.remove(portfolioName);
-        return map;
+    public Map<String, ArrayList<Stock>> deletePortfolio(@PathVariable("portfolioName") String portfolioName) throws Exception {
+        if(map.containsKey(portfolioName)){
+            map.remove(portfolioName);
+            return map;
+        } else {
+            throw new Exception("The portfolio you are trying to delete does not exist. Please try again");
+        }
+
     }
 
     @RequestMapping(method = RequestMethod.DELETE, path = "/api/v1/portfolios/{portfolioName}/{ticker}")
     public Map<String, ArrayList<Stock>> deletePosition(@PathVariable("portfolioName") String portfolioName,
-                                                        @PathVariable("ticker") String ticker){
-
-        ArrayList portfolio = map.get(portfolioName);
-
-        for(int i = 0; i < portfolio.size(); i++){
-            Stock position = (Stock) portfolio.get(i);
-            if((position.getName()).equals(ticker)){
-                portfolio.remove(position);
+                                                        @PathVariable("ticker") String ticker) throws Exception {
+        if(map.containsKey(portfolioName)){
+            ArrayList portfolio = map.get(portfolioName);
+            for(int i = 0; i < portfolio.size(); i++){
+                Stock position = (Stock) portfolio.get(i);
+                if((position.getName()).equals(ticker)){
+                    portfolio.remove(position);
+                }
             }
+            return map;
+        } else {
+            throw new Exception("The position you are trying to delete does not exist in the portfolio. Please try again.");
         }
-        return map;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/**")
+    public void errorHandler() throws Exception {
+        throw new Exception("The url you entered does not exist. Please check it and try again.");
     }
 
 }
